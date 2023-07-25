21b4d2c0b1a6d75b819096013018efdfdcf8dd3e
Rahul_Yadav <ydv.rahul09@gmail.com>
2023-07-26 00:30:16 +0530

    Add to Cart Feature

--- locators/menubar.locator.ts
+++ locators/menubar.locator.ts
@@ -4,9 +4,11 @@ import { test, Locator, Page } from "@playwright/test";
 export default class MenuLocator{
 
     readonly WebsiteLogo : string;
+    
 
     constructor(page: Page){
        this.WebsiteLogo = `#logo`;
+
     }
 
 }
